7440c831db32b62c2eaedc95e67f903825ae5d37
SyKoHPaTh <sykohpath@gmail.com>
2022-10-15 13:28:42 -0400

    Add intro.py, first file of Programming Basics video series

---
+++ intro.py
@@ -0,0 +1,19 @@
+''' Introduction to Programming Basics
+
+	Make for a Youtube Tutorial video.
+
+	Programming Basics Playlist: https://www.youtube.com/playlist?list=PLBaS0R4eUcBe54RYKoeBuzZuXxnWd9JNs
+	[master] https://www.youtube.com/watch?v=-7L12XM2Dig
+
+    License:  Free for Public/Personal/Commercial Use
+
+    Author: SyKoHPaTh
+
+    Shameful Self-Promotion:
+        - Discord   https://discord.gg/UeNKDCJ
+        - Website   http://www.sykohpath.com
+
+    If you have any question/comments/concerns/kittens, please ask on the Discord!
+'''
+
+print('Like and Subscribe')
